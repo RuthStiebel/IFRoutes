b3b16e2f22d3f1a0748a47fcd9f463283992cdbe
Ruth Stiebel <stiebelruth@gmail.com>
2025-11-23 10:55:04 +0200

    modified:   front/.gitignore modified:   front/lib/globals.ts

--- front/lib/globals.ts
+++ front/lib/globals.ts
@@ -1,4 +1,3 @@
-//export const API_URL = "https://ifroutesbackend.onrender.com/api";
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://ifroutesbackend.onrender.com/api";
 export const AIRPORT_ID = "LLBG";
