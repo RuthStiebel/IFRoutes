da26eb23e8f733f819a70f5e9a14cb7e142a0f0c
Ruth Stiebel <stiebelruth@gmail.com>
2025-11-23 10:49:31 +0200

    modified:   front/lib/globals.ts

--- front/lib/globals.ts
+++ front/lib/globals.ts
@@ -1,3 +1,5 @@
-export const API_URL = "https://ifroutesbackend.onrender.com/api";
+//export const API_URL = "https://ifroutesbackend.onrender.com/api";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://ifroutesbackend.onrender.com/api";
 export const AIRPORT_ID = "LLBG";
 export type PracticeMode = "FULL" | "NO_ALT" | "NO_FIX" | "CLEAN";
