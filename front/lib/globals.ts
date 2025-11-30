1166d257e8632b4c5f0d56cacc644363b1401aea
Ruth Stiebel <stiebelruth@gmail.com>
2025-11-30 08:31:17 +0200

    Changed API url to localhost for development 	modified:   front/lib/globals.ts

--- front/lib/globals.ts
+++ front/lib/globals.ts
@@ -1,4 +1,3 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api"; //"https://ifroutesbackend.onrender.com/api";
+export const API_URL = "http://localhost:5000/api"; //  process.env.NEXT_PUBLIC_API_URL || "https://ifroutesbackend.onrender.com/api";
 export const AIRPORT_ID = "LLBG";
 export type PracticeMode = "FULL" | "NO_ALT" | "NO_FIX" | "CLEAN";
